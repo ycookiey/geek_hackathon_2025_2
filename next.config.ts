5f5db27567e9290bf04fc450d656925fa7e39b8c
ycookiey <70356861+ycookiey@users.noreply.github.com>
2025-03-31 03:06:14 +0000

    update:next.config.tsからwebpack設定を削除し、高速化

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    webpack: (config) => {
-        config.watchOptions = {
-            poll: 300,
-            aggregateTimeout: 0,
-        };
-        return config;
-    },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
